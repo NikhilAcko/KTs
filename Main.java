f23fecf2fa4a74ab7dc1944acef626ed06527357
NikhilAcko <113964919+NikhilAcko@users.noreply.github.com>
2022-09-21 23:47:26 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class SeleniumTest {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","/Users/nikhil.kabba/IdeaProjects/SeleniumFirstUseCase/chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+        driver.get("https://www.imdb.com");
         driver.findElement(By.id("suggestion-search")).sendKeys("Spider Man");
         driver.findElement(By.id("suggestion-search-button")).click();
         driver.quit();
