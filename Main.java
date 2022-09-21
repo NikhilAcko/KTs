2e6dce498fa9e0e7d1a5afadb07a5246cdf9f4ab
NikhilAcko <113964919+NikhilAcko@users.noreply.github.com>
2022-09-21 23:38:40 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","/Users/nikhil.kabba/IdeaProjects/SeleniumFirstUseCase/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.findElement(By.id("suggestion-search")).sendKeys("Spider Man");
+        driver.findElement(By.id("suggestion-search-button")).click();
+        driver.quit();
+    }
+}
